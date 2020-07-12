805673144664fca6e65acdcce69b1acc10cbd928
beatorizu <betrizaf@gmail.com>
2020-07-12 18:55:19 -0300

    feat: Star game

--- game.js
+++ game.js
@@ -83,13 +83,66 @@ const flappyBird = {
   }
 }
 
+const getReadyMessage = {
+  spriteX: 134,
+  spriteY: 0,
+  width: 174,
+  height: 152,
+  x: (canvas.width / 2) - 174 / 2,
+  y: 50,
+  draw: () => {
+    context.drawImage(
+      sprites,
+      getReadyMessage.spriteX, getReadyMessage.spriteY,
+      getReadyMessage.width, getReadyMessage.height,
+      getReadyMessage.x, getReadyMessage.y,
+      getReadyMessage.width, getReadyMessage.height,
+    )
+  }
+}
+
+// Scenes
+
+let activeScene = {}
+const changeScene = newScene => activeScene = newScene;
+
+const Scenes = {
+  START: {
+    draw: () => {
+      background.draw();
+      ground.draw();
+      flappyBird.draw();
+      getReadyMessage.draw();
+    },
+    click: () => {
+      changeScene(Scenes.GAME);
+    },
+    update: () => {}
+  },
+  GAME: {
+    draw: () => {
+      background.draw();
+      ground.draw();
+      flappyBird.draw();
+    },
+    update: () => {
+      flappyBird.update();
+    }
+  }
+}
+
 function loop() {
-  background.draw();
-  ground.draw();
-  flappyBird.update();
-  flappyBird.draw();
+  activeScene.draw();
+  activeScene.update();
 
   requestAnimationFrame(loop);
 }
 
+window.addEventListener('click', () => {
+  if (activeScene.click) {
+    activeScene.click();
+  }
+})
+
+changeScene(Scenes.START);
 loop();
